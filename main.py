7dc1f67a5502d8bf54745722f99b6ff1bacdf975
2lu3 <hi2lu3@gmail.com>
2021-06-27 15:20:28 +0900

    グラフを見やすく表示するように

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from math import sqrt, pi, sin, cos
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from tqdm import tqdm
 
 class Consumer:
     def __init__(self, universe):
@@ -26,6 +27,7 @@ class Consumer:
             distance = -1
 
         near_stores = [store for store in self.universe.store_list if sqrt((store.x - self.x)**2 + (store.y - self.y)**2) < distance]
+        print(len(near_stores))
 
         if self.is_application_user == True:
             if len(near_stores):
@@ -41,7 +43,8 @@ class Consumer:
 
         if applying_store != None:
             # self.wanted_mask_number を50の倍数に
-            self.wanted_mask_number = 50 * (self.wanted_mask_number // 50)
+            mask_number = 10
+            self.wanted_mask_number = mask_number* (self.wanted_mask_number // mask_number)
             if self.universe.buying_number_limit == -1:
                 applying_store.request_information.append([self, self.wanted_mask_number])
             else:
@@ -66,12 +69,11 @@ class Store:
         self.y = 0.
 
         self.universe = universe
-        self.initial_mask_number = round(universe.all_supply / universe.store_number * max(gauss(1, 0.2), 1))
+        self.initial_mask_number = round(universe.all_supply / universe.store_number * max(gauss(1, 0.2), 0.1))
         self.posessing_mask_number = self.initial_mask_number
 
     def agt_init(self):
         self.request_information = []
-        self.initial_mask_number = self.posessing_mask_number
         self.time = randint(0, self.universe.mask_storage_period-1)
 
     def agt_step(self):
@@ -97,11 +99,10 @@ class Store:
             else:
                 # 店の在庫が十分にある場合
                 consumer.posessing_mask_number += applying_mask_number
-                consumer.buying_failed_number = 0
                 self.posessing_mask_number -= applying_mask_number
+                consumer.buying_failed_number = 0
                 if self in consumer.blacklist:
                     consumer.blacklist.remove(self)
-
 
 class Universe:
     def __init__(self):
@@ -133,6 +134,7 @@ class Universe:
         self.mask_average = []
         self.mask_std = []
         self.wanted_mask_number_average = []
+        self.store_mask_average = []
 
         # 1ステップあたりの全供給量(需要と供給のバランスをとる,倍率をかける余地あり)
         self.all_supply = round(self.consume_mask_number * self.consumer_number * self.mask_storage_period * self.supply_portion)
@@ -148,7 +150,6 @@ class Universe:
         consumers = self.create_agt(Consumer, num=self.consumer_number)
         store_selection_weights = [store.initial_mask_number for store in self.store_list]
 
-        print(store_selection_weights)
         for consumer in consumers:
             store = choices(self.store_list, weights=store_selection_weights)[0]
             degree = random() * 2 * pi
@@ -188,16 +189,26 @@ class Universe:
         self.mask_average.append(np.average(consumer_mask_list))
         self.mask_std.append(np.std(consumer_mask_list))
         self.wanted_mask_number_average.append( np.average(wanted_mask_list))
+        self.store_mask_average.append(np.average(store_mask_list))
         self.raw_df[self.step] = consumer_mask_list + wanted_mask_list + store_mask_list
         self.step += 1
 
     def univ_finish(self):
         pass
 
+
     def plot(self):
-        for data in [self.mask_average, self.mask_std, self.wanted_mask_number_average]:
+        plt.figure()
+        for consumer in self.consumer_list:
+            plt.scatter(consumer.x, consumer.y, c='black')
+        for store in self.store_list:
+            plt.scatter(store.x, store.y, c="red")
+        plt.figure()
+        #for data in [self.mask_average, self.mask_std, self.wanted_mask_number_average]:
+        for data in [self.mask_average, self.mask_std, self.wanted_mask_number_average, self.store_mask_average]:
             plt.plot([i for i in range(len(data))] , data)
-        plt.legend(['mask mean', 'mask std', 'wanted mean'])
+        #plt.legend(['mask mean', 'mask std', 'wanted mean'])
+        plt.legend(['mask mean', 'mask std', 'wanted mean', 'store mean'])
         plt.show()
 
     def save_df(self, file_name):
@@ -220,19 +231,28 @@ class Universe:
 
 
 def main():
+    seed(1)
     universe = Universe()
-    universe.univ_init()
+    universe.univ_init(consumer_number=20, store_number= 2, consume_mask_number=10)
     for consumer in universe.consumer_list:
         consumer.agt_init()
     for store in universe.store_list:
         store.agt_init()
-    for i in range(100):
+    for _ in tqdm(range(100)):
         universe.univ_step_begin()
         for consumer in universe.consumer_list:
             consumer.agt_step()
         for store in universe.store_list:
             store.agt_step()
         universe.univ_step_end()
+
+        if False:
+            print(universe.step)
+            for consumer in universe.consumer_list:
+                print('con', consumer.posessing_mask_number)
+            for store in universe.store_list:
+                print('sto', store.posessing_mask_number)
+
     universe.plot()
 
 if __name__ == '__main__':
